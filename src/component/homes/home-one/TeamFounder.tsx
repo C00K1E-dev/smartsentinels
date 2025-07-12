1495fab881aedb2f1e8a533a1fdbf5525fcbe914
C00K1E-dev <a.galea@nextgen-it.tech>
2025-07-12 09:02:55 +0300

    add pics update links

--- src/component/homes/home-one/TeamFounder.tsx
+++ src/component/homes/home-one/TeamFounder.tsx
@@ -8,7 +8,9 @@ interface DataType {
    id: number;
    thumb: StaticImageData;
    title: string;
-   designasion: string
+   designasion: string;
+   linkedin: string;
+   twitter: string;
 }[];
 
 const founder_data: DataType[] = [
@@ -17,12 +19,16 @@ const founder_data: DataType[] = [
       thumb: founder_thumb_1,
       title: "Andrei Galea",
       designasion: "Founder & Core Developer",
+      linkedin: "https://www.linkedin.com/in/andrei-galea-a2a8b9278/",
+      twitter: "https://x.com/Andrei___G",
    },
    {
       id: 2,
       thumb: founder_thumb_2,
       title: "Darius Galea",
       designasion: "Co-Founder",
+      linkedin: "https://www.linkedin.com/in/darius-galea-a3072418b/",
+      twitter: ""
    },
 ]
 
@@ -44,20 +50,20 @@ const TeamFounder = () => {
                            <h3 className="founder-card-title">{item.title}</h3>
                            <p className="founder-card-desig">{item.designasion}</p>
                            <div className="social-btn">
-                              <Link href="https://www.linkedin.com/">
-                                 <i className="fab fa-linkedin"></i>
-                              </Link>
-                              <Link href="#">
-                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
-                                    <path
-                                       d="M10.0596 7.34522L15.8879 0.570312H14.5068L9.44607 6.45287L5.40411 0.570312H0.742188L6.85442 9.46578L0.742188 16.5703H2.12338L7.4676 10.3581L11.7362 16.5703H16.3981L10.0593 7.34522H10.0596ZM8.16787 9.54415L7.54857 8.65836L2.62104 1.61005H4.74248L8.71905 7.29827L9.33834 8.18405L14.5074 15.5779H12.386L8.16787 9.54449V9.54415Z"
-                                       fill="currentColor"></path>
-                                 </svg>
-                              </Link>
-                              <Link href="https://youtube.com/">
-                                 <i className="fab fa-youtube"></i>
-                              </Link>
-                           </div>
+   <Link href={item.linkedin} target="_blank" rel="noopener noreferrer">
+      <i className="fab fa-linkedin"></i>
+   </Link>
+   <Link href={item.twitter} target="_blank" rel="noopener noreferrer">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+         <path
+            d="M10.0596 7.34522L15.8879 0.570312H14.5068L9.44607 6.45287L5.40411 0.570312H0.742188L6.85442 9.46578L0.742188 16.5703H2.12338L7.4676 10.3581L11.7362 16.5703H16.3981L10.0593 7.34522H10.0596ZM8.16787 9.54415L7.54857 8.65836L2.62104 1.61005H4.74248L8.71905 7.29827L9.33834 8.18405L14.5074 15.5779H12.386L8.16787 9.54449V9.54415Z"
+            fill="currentColor"></path>
+      </svg>
+   </Link>
+   <Link href="https://youtube.com/" target="_blank" rel="noopener noreferrer">
+      <i className="fab fa-youtube"></i>
+   </Link>
+</div>
                         </div>
                      </div>
                   </div>
